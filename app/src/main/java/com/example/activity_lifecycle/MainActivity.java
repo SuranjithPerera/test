9f1936af5931774d2a8adaa2ce7848d883847ea1
udaya <udayaperera2012@gmail.com>
2024-11-11 15:40:05 +0530

    first commit

--- app/src/main/java/com/example/activity_lifecycle/MainActivity.java
+++ app/src/main/java/com/example/activity_lifecycle/MainActivity.java
@@ -5,9 +5,7 @@ import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
